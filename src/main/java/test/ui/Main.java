a2bcbae7863d5f0ad571820f3ec8e764ec05b594
aleksei <6001mx7@gmail.com>
2022-11-26 22:52:07 +0300

    rename

--- src/main/java/test/ui/Main.java
+++ src/main/java/test/ui/Main.java
@@ -4,13 +4,31 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         //System.out.println("Hello world!");
         Ui ui = new Ui();
+
         ui.startDriver()
                 .openRegForm()
                 .testRegFormWrong();
 
         Thread.sleep(5000);
 
-        //ui.testRegFormGood();
-        //Thread.sleep(5000);
+        ui.openRegForm()
+                .testRegFormGood();
+
+        Thread.sleep(5000);
+        ui.closeDriver();
+
+
+        //--------
+
+        ui.startDriver()
+                .openRegForm()
+                .testLoginFormWrong();
+
+        Thread.sleep(5000);
+
+        ui.testLoginFormGood()
+                .closeDriver();
+
+
     }
 }
--- src/main/java/test/ui/Ui.java
+++ src/main/java/test/ui/Ui.java
@@ -19,7 +19,7 @@ public class Ui {
             return this;
         }
 
-        public void clouseDriver(){
+        public void closeDriver(){
             driver.close();
         }
 
@@ -52,19 +52,59 @@ public class Ui {
             String xPathInputName = "//input[@name='name']";
             String xPathInputEmail = "//input[@name='email']";
             String xPathInputPassword = "//form[@action='/user/register/index.html']//input[@type='password']";
-            String xPathErrorText = "//form/p[contains(text(), 'Неправильное значение поля')]";
+            //String xPathErrorText = "//form/p[1]";
 
             WebElement elementForm = driver.findElement(By.xpath(xPathForm));
             WebElement elementInputName = elementForm.findElement(By.xpath(xPathInputName));
             WebElement elementInputEmail = elementForm.findElement(By.xpath(xPathInputEmail));
             WebElement elementInputPassword = driver.findElement(By.xpath(xPathInputPassword));
-            WebElement elementErrorText = driver.findElement(By.xpath(xPathErrorText));
+            //WebElement elementErrorText = driver.findElement(By.xpath(xPathErrorText));
 
             elementInputName.sendKeys(name);
             elementInputEmail.sendKeys(email);
             elementInputPassword.sendKeys(password);
 
-            System.out.println(elementErrorText.getText());
+            //System.out.println(elementErrorText.getText());
+
+            return this;
+        }
+
+        public Ui typeLoginForm(String login, String password){
+            String xPathForm = "//form[@action='/user/login/index.html']";
+            String xPathRegText = "//h2[text()='Вход']";
+            String xPathInputLogin = "//input[@name='login']";
+            String xPathInputPassword = "//input[@type='password']";
+
+            WebElement elementForm = driver.findElement(By.xpath(xPathForm));
+            WebElement elementInputLogin = elementForm.findElement(By.xpath(xPathInputLogin));
+            WebElement elementInputPassword = elementForm.findElement(By.xpath(xPathInputPassword));
+
+            elementInputLogin.sendKeys(login);
+            elementInputPassword.sendKeys(password);
+
+            return this;
+        }
+
+        public Ui testLoginFormGood(){
+
+            typeLoginForm("my@email.com", "MyPass123");
+            submitLoginForm();
+
+            return this;
+        }
+
+        public Ui testLoginFormWrong(){
+
+            typeLoginForm("myail.com", "MyPass123");
+            submitLoginForm();
+
+            return this;
+        }
+
+        public Ui submitLoginForm(){
+            String xPathInputSubmit = "//input[@value='Авторизоваться']";
+            WebElement element = driver.findElement(By.xpath(xPathInputSubmit));
+            element.click();
 
             return this;
         }
