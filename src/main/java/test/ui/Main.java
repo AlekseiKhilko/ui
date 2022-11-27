f5b79a647f52b0b8aed5b36fd3aeededc9e69e6b
aleksei <6001mx7@gmail.com>
2022-11-27 21:51:33 +0300

    add comments

--- src/main/java/test/ui/Main.java
+++ src/main/java/test/ui/Main.java
@@ -5,30 +5,36 @@ public class Main {
         //System.out.println("Hello world!");
         Ui ui = new Ui();
 
+        //test reg form wrong
         ui.startDriver()
                 .openRegForm()
                 .testRegFormWrong();
 
+        //sleep
         Thread.sleep(5000);
 
+        //test reg form good
         ui.openRegForm()
                 .testRegFormGood();
 
+        //sleep
         Thread.sleep(5000);
-        ui.closeDriver();
 
+        ui.closeDriver();
 
         //--------
 
+        //test login form wrong
         ui.startDriver()
                 .openRegForm()
                 .testLoginFormWrong();
 
+        //sleep
         Thread.sleep(5000);
 
+        //test login form good
         ui.testLoginFormGood()
                 .closeDriver();
-
 
     }
 }
